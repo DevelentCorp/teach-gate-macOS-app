c5ccbeda48b612fe9ca7a07fa540130a0c7afa1d
obedsayyad <obedsayyad123@gmail.com>
2025-08-25 09:31:52 +0530

    WIP: Current changes after Xcode build attempt

--- react-native.config.js
+++ react-native.config.js
@@ -4,5 +4,11 @@ module.exports = {
       scheme: 'Teach Gate-macOS',
     },
   },
+  // Disable autolinking for the local Outline VPN package to avoid duplicate sources with Podfile path override
+  dependencies: {
+    'react-native-outline-vpn': {
+      platforms: {ios: null, macos: null},
+    },
+  },
   assets: ['./src/assets/fonts/'],
 };
