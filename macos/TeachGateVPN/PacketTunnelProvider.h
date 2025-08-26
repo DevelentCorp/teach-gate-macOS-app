1956a753e05757d85e85bddcf5cf92c5050d4aef
obedsayyad <obedsayyad123@gmail.com>
2025-08-26 22:41:08 +0530

    Latest updates: outline integration and new OutlineAppleLib added to macos

---
+++ macos/TeachGateVPN/PacketTunnelProvider.h
@@ -0,0 +1,12 @@
+//
+//  PacketTunnelProvider.h
+//  TeachGateVPN
+//
+//  Created by Obed Sayyad on 26/08/25.
+//
+
+#import <NetworkExtension/NetworkExtension.h>
+
+@interface PacketTunnelProvider : NEPacketTunnelProvider
+
+@end
